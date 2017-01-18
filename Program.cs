a6877e394275e7baf0f2112febeb6ada314c14b1
AntonioTomo01 <AntonioTomo01@gmail.com>
2017-01-18 11:40:36 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace housePRice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double naiMalkaStaya = double.Parse(Console.ReadLine());
+            double kuhnya = double.Parse(Console.ReadLine());
+            double cenaKvadratenMetur = double.Parse(Console.ReadLine());
+
+            double banya = naiMalkaStaya / 2;
+            double vtoraStaya = (naiMalkaStaya / 100) * 110;
+            double tretaStaya = (vtoraStaya / 100) * 110;
+            double obshtaPlosht = naiMalkaStaya + kuhnya + banya + vtoraStaya + tretaStaya;
+            double obshtaPloshtSkoridor = (obshtaPlosht / 100) * 105;
+
+            double cena = cenaKvadratenMetur * obshtaPloshtSkoridor;
+
+            Console.WriteLine("{0:F2}", cena);
+        }
+    }
+}
